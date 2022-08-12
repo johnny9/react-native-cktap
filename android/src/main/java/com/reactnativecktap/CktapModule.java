b81b684f67c470a985d67f21b9e909d957a97bed
John Carlson <JohnCarlson@pm.me>
2022-08-11 22:58:11 -0400

    feat(android): add activityeventlistener

--- android/src/main/java/com/reactnativecktap/CktapModule.java
+++ android/src/main/java/com/reactnativecktap/CktapModule.java
@@ -1,25 +1,45 @@
 package com.reactnativecktap;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.util.Log;
+import android.content.Intent;
+import android.app.Activity;
+import android.nfc.tech.IsoDep;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.module.annotations.ReactModule;
 
 @ReactModule(name = CktapModule.NAME)
-public class CktapModule extends ReactContextBaseJavaModule {
+public class CktapModule extends ReactContextBaseJavaModule implements ActivityEventListener {
     public static final String NAME = "Cktap";
 
     public CktapModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        reactContext.addActivityEventListener(this);
     }
 
     @Override
     @NonNull
     public String getName() {
         return NAME;
+    }
+
+     /** Called when host (activity/service) receives an {@link Activity#onActivityResult} call. */
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(NAME, "onActivityResult");
+    }
+
+    /** Called when a new intent is passed to the activity */
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.d(NAME, "onNewIntent");
+
     }
 
 
